4d5fa2fbd91e21f511e5e9fcbf6b85d28549b8f6
rain892 <33967269+rain892@users.noreply.github.com>
2021-10-24 16:51:22 -0700

    Add files via upload

---
+++ C_11_Unindented_code_2-main/sketch.js
@@ -0,0 +1,50 @@
+
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+//create a image 
+
+function preload() {
+trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+trex_collided = loadImage("trex_collided.png");
+groundImage = loadImage("ground2.png")
+}
+
+function setup()
+ {
+createCanvas(600, 200);
+//create a trex sprite
+
+trex = createSprite(50,160,20,50);
+trex.addAnimation("running", trex_running);
+trex.scale = 0.5;
+//create a ground sprite
+
+ground = createSprite(200,180,400,20);
+ground.addImage("ground",groundImage);
+ground.x = ground.width /2;
+ground.velocityX = -4;
+
+invisibleGround = createSprite(300,185,600,10)
+invisibleGround.visible = false
+}
+
+function draw() {
+background(220);
+//jump when the space button is pressed
+console.log(trex.y)
+
+if (keyDown("space" )&& trex.y >= 156.5 ) {
+
+    trex.velocityY = -10;
+}
+
+//When the trex jumps
+
+trex.velocityY = trex.velocityY + 0.8
+if (ground.x < 0) {
+    ground.x = ground.width / 2;
+}
+trex.collide(invisibleGround);
+
+drawSprites();
+}
